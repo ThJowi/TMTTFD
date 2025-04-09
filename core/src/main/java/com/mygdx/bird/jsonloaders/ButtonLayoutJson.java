bcf964273b196ecbd280fe3e48f34ab81b6e8c1e
ThJowi <ThJowi>
2025-04-09 17:52:32 +0200

    starting

---
+++ core/src/main/java/com/mygdx/bird/jsonloaders/ButtonLayoutJson.java
@@ -0,0 +1,12 @@
+package com.mygdx.bird.jsonloaders;
+
+import java.util.ArrayList;
+
+public class ButtonLayoutJson
+{
+    public ArrayList<ButtonJson> buttons;
+
+    public ButtonLayoutJson()
+    {
+    }
+}
